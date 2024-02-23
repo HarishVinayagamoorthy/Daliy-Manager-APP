270ca225ce5704155d9fa57072702525550d8553
HarishVinayagamoorthy <harishvinayagamoorthy123@gmail.com>
2024-02-23 09:40:01 +0530

    code updated

--- src/routes/TodoRoutes.js
+++ src/routes/TodoRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import TodoController from "../controller/TodoController.js"
-import Auth from "../common/auth.js"
+import Auth from "../common/Auth.js"
 
 const router=express.Router()
 
